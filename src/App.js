450e3d1f86bdc13d728a794cf68d57d7650399ac
jgoricha <92893208+jgoricha@users.noreply.github.com>
2024-07-03 11:55:28 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
 import React from 'react';
-
 import 'bootstrap/dist/css/bootstrap.min.css';
-
 //Code to import Budget.js
 import Budget from './components/Budget';
-import Remaining from './components/Remaining';
-
-
+// Add code to import the other components here under
 import { AppProvider } from './context/AppContext';
 const App = () => {
     return (
@@ -14,28 +10,101 @@ const App = () => {
             <div className='container'>
                 <h1 className='mt-3'>Company's Budget Allocation</h1>
                     <div className='row mt-3'>
-                        <div className='col-sm'>
-                            <Budget />
-                        </div>      
-                        <div className='col-sm'>
-                            <Remaining />
-                        </div>    
-                        <div className='col-sm'>
-                            <ExpenseTotal />
-                        </div>        
-                       
-                        {
-                            /* Add ExpenseList component here */
-                        }         
+                        {/* Add Budget component here under */}
+                        {/* Add Remaining component here under */}
+                        {/* Add ExpenseTotal component here under */}
+                        {/* Add ExpenseList component here under */}
+                        {/* Add ExpenseItem component here under */}
+                        {/* Add AllocationForm component here under */}
+                </div>
+            </div>
+        </AppProvider>
+    );
+};
+export default App;
+Copied!
+Click here to view the hint
+Click here to view the solution
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+26
+27
+28
+29
+30
+31
+32
+33
+34
+35
+36
+37
+38
+39
+40
+41
+42
+43
+44
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { AppProvider } from './context/AppContext';
+import Budget from './components/Budget';
+import ExpenseTotal from './components/ExpenseTotal';
+import ExpenseList from './components/ExpenseList';
+import AllocationForm from './components/AllocationForm';
+import RemainingBudget from './components/Remaining';
 
-                        {
-                            /* Add ExpenseItem component here */
-                        }        
-
-                        {
-                            /* Add AllocationForm component here under */
-                        }        
-
+const App = () => {
+    return (
+        <AppProvider>
+            <div className='container'>
+                <h1 className='mt-3'>Company's Budget Allocation</h1>
+                <div className='row mt-3'>
+                    <div className='col-sm'>
+                        <Budget />
+                    </div>
+                    <div className='col-sm'>
+                        <RemainingBudget />
+                    </div>
+                    <div className='col-sm'>
+                        <ExpenseTotal />
+                    </div>
+                </div>
+                <h3 className='mt-3'>Allocation</h3>
+                <div className='row '>
+                    <div className='col-sm'>
+                        <ExpenseList />
+                    </div>
+                </div>
+                <h3 className='mt-3'>Change allocation</h3>
+                <div className='row mt-3'>
+                    <div className='col-sm'>
+                        <AllocationForm/>
+                    </div>
                 </div>
             </div>
         </AppProvider>
